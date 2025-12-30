2543dbbc2f3992f338b74d63c302b0922a9e14fc
josmanov <jusuf.osmanovic01@gmail.com>
2025-12-30 17:50:57 +0200

    add moving asteroid shapes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import constants
 from logger import log_state
 from player import Player
 import circleshape
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
@@ -10,12 +12,26 @@ def main():
     print("Screen height:", constants.SCREEN_HEIGHT)
 
     pygame.init()
+
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+   
+    Asteroid.containers = (asteroids, updatable, drawable)
+
     Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    # asteroids = Asteroid(
+    #    constants.SCREEN_WIDTH / 2,
+    #    constants.SCREEN_HEIGHT / 2,
+    #    constants.ASTEROID_MIN_RADIUS
+    #    )
+    asteroid_field = AsteroidField()
 
     while (1):
         for event in pygame.event.get():
@@ -28,6 +44,8 @@ def main():
         screen.fill("black", None, 0)
         for item in drawable:
             item.draw(screen)
+        for item in updatable:
+            item.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
-from circleshape import CircleShape
 import constants
+from circleshape import CircleShape
 
 class Player(CircleShape):
     def __init__(self, x, y):
